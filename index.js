be84bd1c577dccebbbb02c3b99c4ca80cf4d7971
root <root@vps241870.ovh.net>
2016-02-09 17:20:12 +0100

    live on server'

--- index.js
+++ index.js
@@ -61,6 +61,6 @@ app.use("/", indexRoutes);
 app.use("/", newsRoutes);
 
 //server launch
-app.listen(process.env.PORT, process.env.IP, function(){
+app.listen(3000, function(){
     console.log('Serveur en route!');
 });
